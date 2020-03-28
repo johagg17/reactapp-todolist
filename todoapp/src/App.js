4703fa1202a2d591311e6e8bce7c28cfe55eed40
Johannes <johagg17@student.hh.se>
2020-03-28 20:20:21 +0100

    commited some files

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,70 +1,103 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-//import firebase from './firebase'
+import firebase from './firebase'
+
 /* Destructing props:
     instead of function(props) we can use function({fName, lName, age})
-
 */
+
 /* function todo that takes in props but destructed
   it returns an div element which inludes the given row(number) and the text for the todo
 */
 
-function Todo({todo, index, completeTodo, removeTodo}){
-  return(
-  <div style={{textDecoration: todo.isCompleted ? 'line-through': 'none'}} className = "todo">
-  {index+1}{' '}{todo.text}
-    <div>
-      <input type = "checkbox" onChange = {() => completeTodo(index)}></input>
-      <button onClick = {() => removeTodo(index)}>x</button>
+function Todo({ todo, index, completeTodo, removeTodo }) { // (props) = {todo, index, completeTodo, removeTodo}
+  return (
+    <div style={{ textDecoration: todo.isCompleted ? 'line-through' : 'none' }} className="todo">
+      {index + 1}{' '}{todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Completed</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
     </div>
-  </div>
   )
 }
 
-function TodoForm({addTodo}){
+function TodoForm({ addTodo }) { // props = {addTodo}
   const [value, setValue] = useState('');
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    if(!value) return; // Just pressing enter without any text
+    if (!value) return; // Just pressing enter without any text
     addTodo(value);
     setValue('');
   }
-  function change(e){
+  function change(e) {
     setValue(e.target.value); // e.target returns the value which got triggerd setValue is the function for setting value
   }
   return (
-    <form onSubmit = {handleSubmit}>
-      <input type = "text" className= "input" value = {value} placeholder="Add Todo..." onChange = {change}/>
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} placeholder="Add Todo..." onChange={change} />
     </form>
   )
 }
 function App() {
   const [todos, setTodos] = useState([]);
-  function addTodo(text){
+  useEffect(()=>{
+    const unsubcribe = firebase
+    .firestore()
+    .collection('time')
+    .onSnapshot((snapshot) =>{
+      const newTimes = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+      setTodos(newTimes);
+    });
+    return () => unsubcribe()
+  },[]);
+  function addTodo(text) {
     const newvalue = {
+      id: todos.length + 1,
       text: text,
       isCompleted: false
     };
+    const unsubcribe = firebase
+    .firestore()
+    .collection('time').doc(newvalue.id + "")
+    .set(newvalue);
     const newTodolist = [...todos, newvalue];
     setTodos(newTodolist);
+    return () => unsubcribe();
   }
-  function completeTodo(index){
+
+  function completeTodo(index) {
     const newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    const unsubcribe = firebase
+    .firestore()
+    .collection('time')
+    .doc(newTodos[index].id + "").update({isCompleted:true});
+    newTodos[index].isCompleted = true;
     setTodos(newTodos);
   }
-  function removeTodo(index){
+
+  function removeTodo(index) {
     const newTodos = [...todos];
-    newTodos.splice(index, 1);
+    const removed = newTodos.splice(index, 1);
+    console.log(removed[0].id);
+    const unsubcribe = firebase
+    .firestore()
+    .collection('time')
+    .doc(removed[0].id +"").delete();
     setTodos(newTodos);
+    return () => unsubcribe(); 
   }
-  return(
-    <div className = "app">
-      <div className = "todo-list">
+
+  return (
+    <div className="app">
+      <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo index = {index}  todo = {todo} completeTodo = {completeTodo} removeTodo = {removeTodo}/>
+          <Todo index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo} />
         ))}
-        <TodoForm addTodo = {addTodo}/>
+        <TodoForm addTodo={addTodo} />
 
       </div>
     </div>
